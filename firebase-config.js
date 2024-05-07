62d8e9352a4dc9ab875967d6391979117ac2a75c
Marcos <marcosceseus123@gmail.com>
2024-05-06 20:45:19 -0600

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLnO41QQGk1es9Mz_sB6lLGmXDTLx69aw",
+  authDomain: "saferide-3933e.firebaseapp.com",
+  projectId: "saferide-3933e",
+  storageBucket: "saferide-3933e.appspot.com",
+  messagingSenderId: "329735677857",
+  appId: "1:329735677857:web:c6cfb2562c38e565da8de0"
+};
+
+// Initialize Firebase
